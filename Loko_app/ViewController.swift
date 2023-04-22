637a23c8a0a8a25e15a81ac7b28ea73bc322937f
Surkov <nikita16.01.0371@gmail.com>
2023-04-22 12:35:16 +0300

    Initial Commit

---
+++ Loko_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loko_app
+//
+//  Created by Никита Сурков on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
